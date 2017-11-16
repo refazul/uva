82e6d8421078e1bd2379574a95d17e22b8c6746c
Refazul Refat <refazul.refat@gmail.com>
2017-11-16 13:52:40 +0600

    init

---
+++ 10071.cpp
@@ -0,0 +1,8 @@
+// 10071 - Back to High School Physics
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1012
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
